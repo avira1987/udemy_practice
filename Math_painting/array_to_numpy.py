bcd9e25b1701edf4cd9081c6eafc96f428f9c3f5
amir avira <amir.avira1987@gamil.com>
2023-01-02 20:28:29 +0330

    convert design.txt to empty code and compelet clas

--- Math_painting/array_to_numpy.py
+++ Math_painting/array_to_numpy.py
@@ -1,4 +1,17 @@
 import numpy as np
 from PIL import Image
 
-data = np.zeros((5, 4, 3))
+#Create 3d numpy array of zeros
+data = np.zeros((5, 4, 3), dtype= np.uint8)
+data[:]=[255 ,255 , 0]
+print(data)
+#make red  patach
+data[1:4, 1:3]=[255, 200, 233]
+data[3:4, 1:4] = [45, 3, 233]
+ 
+img = Image.fromarray(data, 'RGB')
+img.save('canvas.png')
+
+
+
+
