3474ad70b27ef201875bcc6b99bd723e53ae8a2a
amir avira <amir.avira1987@gamil.com>
2023-01-10 14:27:16 +0330

    photho_searcher last update

---
+++ Webcam_Photo_Sharer/main.py
@@ -0,0 +1,40 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+import time
+from FileSharer import FileShare
+
+Builder.load_file('frontend.kv')
+
+class CameraScreen(Screen):
+    def start(self):
+        self.ids.camera.play = True
+        self.ids.camera_button.text = "Stop Camera"
+        self.ids.camera.texture = self.ids.camera._camera.texture
+    def stop(self):
+        self.ids.camera.play =False
+        self.ids.camera_button.text = "Start Camera"
+        self.ids.camera.texture = None
+
+    def capture(self):
+        current_time = time.strftime("%Y%m%d-%H%M%S")
+        self.filepath = f"files/{ current_time }.png"
+        self.ids.camera.export_to_png(self.filepath)
+        self.manager.current = 'image_screen'
+        self.manager.current_screen.ids.img.source = self.filepath
+        
+class ImageScreen(Screen):
+    def create_link(self):
+        file_path = App.get_running_app().root.ids.camera_screen.filepath
+        print(file_path)
+
+
+class RootWidget(ScreenManager):
+    pass
+
+class MainApp(App):
+
+    def build(self):
+        return RootWidget()
+
+MainApp().run()
