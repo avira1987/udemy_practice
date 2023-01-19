c5a20142390f6f046a58e72b5829782f59d4c76a
amir avira <amir.avira1987@gamil.com>
2023-01-19 05:38:03 +0330

    create navbar menu

--- Instant_dictionary/webapp/dictionary.py
+++ Instant_dictionary/webapp/dictionary.py
@@ -11,18 +11,21 @@ class Dictionary:
         jp.Div(a=div, text="Get the definition of any English word as you type.",
                 classes = "text-lg")
         input_div = jp.Div(a=div, classes ="grid grid-cols-2")
-        input_box =jp.Input(a=input_div , placeholder = "type in a word here.", 
+        output_div = jp.Div(a=div, classes ="m-2 p-2 text-lg border-2 h-40 ")
+
+        input_box =jp.Input(a=input_div , placeholder = "type in a word here.",
+                outputdiv = output_div,
                 classes ="m-2 bg-gray-100 border-2 border-gray-200 "
                     "rounded w-64 focus:bg-white focus:outline-none "
                     "focus:border-purple-500 py-2 px-4")
-        output_div = jp.Div(a=div, classes ="m-2 p-2 text-lg border-2 h-40 ")
+        input_box.on('input', cls.get_definition)
 
-        jp.Button(a=input_div, click=cls.get_definition , text="Get Definition", 
-                outputdiv=output_div ,classes = "border-2 text-gray-500",
-                inputbox= input_box)
+        # jp.Button(a=input_div, click=cls.get_definition , text="Get Definition", 
+        #         outputdiv=output_div ,classes = "border-2 text-gray-500",
+        #         inputbox= input_box)
 
         return wp
     @staticmethod
     def get_definition(widget, msg):
-        defined = Definition(widget.inputbox.value).get()
+        defined = Definition(widget.value).get()
         widget.outputdiv.text = " ".join(defined)
